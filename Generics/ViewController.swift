48fbcad6698198f630c8d7cdee48bac3a5beb8fb
Sean Hendrix <hendoware@gmail.com>
2018-11-27 18:48:46 -0500

    first commit

---
+++ Generics/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Generics
+//
+//  Created by Sean Hendrix on 11/27/18.
+//  Copyright © 2018 Sean Hendrix. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
